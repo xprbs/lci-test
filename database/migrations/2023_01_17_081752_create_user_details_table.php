c81264cf73371f569f8e7cd82351236a805ec325
Pandu Prabu <xprbs.me@gmail.com>
2023-01-17 15:22:12 +0700

    add: migration and model user_details

---
+++ database/migrations/2023_01_17_081752_create_user_details_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->text('address');
+            $table->string('gender', 10);
+            $table->string('image')->nullable()->default(null);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
